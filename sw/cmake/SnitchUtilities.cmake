84d87430a0550fd59523ce622150878198226428
Viviane Potocnik <vivianep@ethz.ch>
2022-05-31 08:49:12 +0200

    sw: adapted CMAKE flow for old banshee version

--- sw/cmake/SnitchUtilities.cmake
+++ sw/cmake/SnitchUtilities.cmake
@@ -5,7 +5,7 @@
 # Locate the banshee simulator for test execution.
 set(SNITCH_BANSHEE "${CMAKE_CURRENT_SOURCE_DIR}/../banshee/target/debug/banshee" CACHE PATH "Path to the banshee simulator for testing")
 set(BANSHEE_TIMEOUT "360" CACHE STRING "Timeout when running tests on banshee")
-set(RUN_BANSHEE_ARGS "--num-cores=9" "--num-clusters=2" "--base-hartid=1" "--train-file-path=/home/msc22f11/PlayGround/mnist_full.csv" "--start-address=0x8004000" CACHE PATH "Arguments passed to the banshee sim for the run-banshee target")
+set(RUN_BANSHEE_ARGS "--num-cores=9" "--num-clusters=2" "--base-hartid=1" CACHE PATH "Arguments passed to the banshee sim for the run-banshee target")
 set(SNITCH_RUNTIME "snRuntime-banshee" CACHE STRING "Target name of the snRuntime flavor to link against")
 set(SNITCH_SIMULATOR "" CACHE PATH "Command to run a binary in an RTL simulation")
 set(SIMULATOR_TIMEOUT "1800" CACHE STRING "Timeout when running tests on RTL simulation")
