67bd55f27543bf6a9def2c5455eedf446c869b1f
Viviane Potocnik <vivianep@ethz.ch>
2022-06-15 16:10:48 +0200

    sw: test FP64 SSR Gradient Update with new setup

--- sw/applications/src/layers/mnist.c
+++ sw/applications/src/layers/mnist.c
@@ -22,7 +22,7 @@
 
 // define which parts of the network to run
 #define RUN_FEEDFORWARD 1
-#define RUN_GRADIENT_UPDATE 0
+#define RUN_GRADIENT_UPDATE 1
 #define RUN_TRAINING_STEP 0
 
 void mnist(const network_t *n){
