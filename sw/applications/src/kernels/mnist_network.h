0463f685a39e1f35dabea8d1ac063030bd8b715c
Viviane Potocnik <vivianep@ethz.ch>
2022-06-20 14:55:26 +0200

    add FP16 SIMD SSR FF (bug: race conditions)

--- sw/applications/src/kernels/mnist_network.h
+++ sw/applications/src/kernels/mnist_network.h
@@ -129,6 +129,11 @@ void feedforward_fp16_ssr_simd(uint32_t IN_CH1, uint32_t IN_CH2, uint32_t OUT_CH
                 uint32_t ldB, __fp16 *image, uint32_t ldI, uint32_t compute_id, uint32_t* core_sync,
                 uint32_t setup_SSR);
 
+void gradient_update_fp16_ssr_simd(uint32_t IN_CH1, uint32_t IN_CH2, uint32_t OUT_CH, 
+                float *weight_grads, uint32_t ldW, float *bias_grads, float *activations, 
+                uint32_t ldB, float *image, uint32_t *target, uint32_t ldI, 
+                uint32_t compute_id, float *loss, uint32_t compute_num, uint32_t setup_SSR);
+
 // INFO Function calls for FP16 WITHOUT SSRs and SIMD
 void feedforward_fp16(uint32_t IN_CH1, uint32_t IN_CH2, uint32_t OUT_CH, 
                 __fp16 *weights, uint32_t ldW, __fp16 *biases, __fp16 *activations,
