43fd88e8bd21c793834d2e0b7de2aea2f0d1a18f
Viviane Potocnik <vivianep@ethz.ch>
2022-06-22 11:20:34 +0200

    sw: manual Conv2D computation for reference implementation

--- sw/applications/include/layer.h
+++ sw/applications/include/layer.h
@@ -249,3 +249,31 @@ typedef struct conv_layer_struct {
 
     precision_t dtype;
 } conv_layer;
+
+typedef struct cnn_layer_struct {
+    // CNN
+    uint32_t CO;
+    uint32_t CI;
+    uint32_t IH;
+    uint32_t IW;
+    uint32_t OH;
+    uint32_t OW;
+    uint32_t FH;
+    uint32_t FW;
+    uint32_t pad;
+
+    double *ifmap;
+    double *weights;
+    double *biases;
+    double *ofmap;
+
+    uint32_t TILE_CI;
+    uint32_t cluster2cluster;
+    uint32_t im2col;
+
+    // BATCHNORM
+    double *gamma;
+    double *beta;
+
+    precision_t dtype;
+} cnn_layer;
