1e4c0d7e22622dc22e3a3f90bf2f432e921b2926
Viviane Potocnik <vivianep@ethz.ch>
2022-07-03 13:42:26 +0200

    sw: change memory layout for FP32 network

--- sw/applications/src/layers/mnist_fp32.c
+++ sw/applications/src/layers/mnist_fp32.c
@@ -16,12 +16,12 @@
 #define MAT_ROW_PADDING 0
 
 // define whether to run baseline network or not
-#define BASELINE 0
+#define BASELINE 1
 
 // define which parts of the network to run
 #define RUN_FEEDFORWARD 1
-#define RUN_GRADIENT_UPDATE 1
-#define RUN_TRAINING_STEP 1
+#define RUN_GRADIENT_UPDATE 0
+#define RUN_TRAINING_STEP 0
 
 void mnist_fp32(const network_fp32_t *n){
 
@@ -64,30 +64,78 @@ void mnist_fp32(const network_fp32_t *n){
     // learning rate of the network
     uint32_t lr_size = sizeof(float);
 
-    // INFO FP64 cluster memory setup
+    // cluster 0 variabels:
+    float *weights_cl0;
+    float *weight_grads_cl0;
+    float *biases_cl0;
+    float *images;
+    float *activations_cl0;
+    float *max;
+
+    // cluster 1 variabels:
+    float *weights_cl1; // dummy variable to match offsets with cluster 0
+    float *weight_grads_cl1;
+    float *bias_grads_cl1;
+    float *activations_cl1;
+    float *loss;
+    float *targets;
+
+    // INFO FP32 cluster memory setup
     // @brief Cluster Memory Structure for each cluster to ensure
     // we can access the data of both by using the constant
     // cluster base offset
-    void *ptr = (float *)snrt_cluster_memory().start;
+    void *ptr = (double *)snrt_cluster_memory().start;
     // void *ptr_start = ptr;
-    float *max= ptr; // zero initialized
-    ptr += max_size;
-    float *loss = ptr; // zero initialized
-    ptr += loss_size;
-    float *images = ptr;
-    ptr += number_of_images*image_size;
-    float *biases = ptr; // bias GRADIENTS zero initialized
-    ptr += bias_mat_size;
-    float *activations = ptr;
-    ptr += act_mat_size;
-    float *weights = ptr; // weight GRADIENTS zero initialized
-    ptr += weight_mat_size;
-    // NOTE: core sync flag used to indictae whether computation is done or not
-    // WARN: fails in the RTL
-    uint32_t *core_sync = ptr; // zero initialized
-    ptr += core_sync_flag_size;
-    uint32_t *targets = ptr;
-    ptr += number_of_images*target_size;
+    if(cluster_id == 0){
+        weights_cl0 = ptr;
+        ptr += weight_mat_size;
+        weight_grads_cl0 = ptr;
+        ptr += weight_mat_size;
+        biases_cl0 = ptr;
+        ptr += bias_mat_size;
+        images = ptr;
+        ptr += image_size * number_of_images;
+        // INFO: the activations are also used for the bias gradients
+        activations_cl0 = ptr;
+        ptr += act_mat_size;
+        max = ptr;
+        ptr += max_size;
+    } else if (cluster_id == 1){
+        weights_cl1 = ptr;
+        ptr += weight_mat_size;
+        weight_grads_cl1 = ptr;
+        ptr += weight_mat_size;
+        bias_grads_cl1 = ptr;
+        ptr += bias_mat_size;
+        images = ptr;
+        ptr += image_size * number_of_images;
+        activations_cl1 = ptr;
+        ptr += act_mat_size;
+        loss = ptr;
+        ptr += loss_size;
+        targets = ptr;
+        ptr += target_size * number_of_images;
+    }
+    // void *ptr = (float *)snrt_cluster_memory().start;
+    // // void *ptr_start = ptr;
+    // float *max= ptr; // zero initialized
+    // ptr += max_size;
+    // float *loss = ptr; // zero initialized
+    // ptr += loss_size;
+    // float *images = ptr;
+    // ptr += number_of_images*image_size;
+    // float *biases = ptr; // bias GRADIENTS zero initialized
+    // ptr += bias_mat_size;
+    // float *activations = ptr;
+    // ptr += act_mat_size;
+    // float *weights = ptr; // weight GRADIENTS zero initialized
+    // ptr += weight_mat_size;
+    // // NOTE: core sync flag used to indictae whether computation is done or not
+    // // WARN: fails in the RTL
+    // uint32_t *core_sync = ptr; // zero initialized
+    // ptr += core_sync_flag_size;
+    // uint32_t *targets = ptr;
+    // ptr += number_of_images*target_size;
     // NOTE: following lines for debugging purposes only
     // void *ptr_end = (double *)snrt_cluster_memory().end;
     // if(compute_id == 0){   
@@ -103,7 +151,9 @@ void mnist_fp32(const network_fp32_t *n){
     uint32_t setup_SSR;
     if(BASELINE){
         // baseline does *not* use SSRs
-        setup_SSR = 0;
+        // setup_SSR = 0;
+        // For the RTL we have to use also the DMA transfer ...
+        setup_SSR = 1;
     } else {
         // define whether SSRs should be used or not
         setup_SSR = 1;
@@ -116,14 +166,14 @@ void mnist_fp32(const network_fp32_t *n){
         snrt_dma_start_tracking();
                 // load initial biases from Golden Model into Cluster 0 memory
                 snrt_dma_txid_t txid_B = 
-                    snrt_dma_start_1d(biases,                 // destination
+                    snrt_dma_start_1d(biases_cl0,                 // destination
                                     n->b,                     // source
                                     n->dtype * n->OUT_CH);    // size
 
                 // load weight data into Cluster 0 memory
                 // TODO: make this 1D DMA transfer
                 snrt_dma_txid_t txid_W = 
-                    snrt_dma_start_2d(weights,                // destination
+                    snrt_dma_start_2d(weights_cl0,                // destination
                                     n->W,                     // source
                                     n->dtype * IN_CH,         // size
                                     n->dtype * IN_CH ,        // destination stride
@@ -189,20 +239,20 @@ void mnist_fp32(const network_fp32_t *n){
                     // INFO: baseline
                     benchmark_get_cycle();
                     feedforward_fp32n(n->IN_CH1, n->IN_CH2, div, 
-                                    &weights[W_offset], ldW, &biases[b_offset], &activations[b_offset],
+                                    &weights_cl0[W_offset], ldW, &biases_cl0[b_offset], &activations_cl0[b_offset],
                                     ldB, &images[curr_img], ldI, compute_id);
                     softmax_activation_fp32n(n->IN_CH1, n->IN_CH2, div, 
-                                &weights[W_offset], ldW, &activations[b_offset], ldB,
+                                &weights_cl0[W_offset], ldW, &activations_cl0[b_offset], ldB,
                                 &images[curr_img], ldI, compute_id, compute_num, max);
                     benchmark_get_cycle();
                 } else {
                     // INFO: FP64 with SSRs
                     benchmark_get_cycle();
                     feedforward_fp32_ssr_simd_frep(n->IN_CH1, n->IN_CH2, div, 
-                                        &weights[W_offset], ldW, &biases[b_offset], &activations[b_offset],
+                                        &weights_cl0[W_offset], ldW, &biases_cl0[b_offset], &activations_cl0[b_offset],
                                         ldB, &images[curr_img], ldI, compute_id, setup_SSR);
                     softmax_activation_fp32(n->IN_CH1, n->IN_CH2, div, 
-                                &weights[W_offset], ldW, &activations[b_offset], ldB,
+                                &weights_cl0[W_offset], ldW, &activations_cl0[b_offset], ldB,
                                 &images[curr_img], ldI, compute_id, compute_num, max);
                     benchmark_get_cycle();
                 }
@@ -244,12 +294,12 @@ void mnist_fp32(const network_fp32_t *n){
             if(snrt_is_dm_core() && cluster_id==1) {
                 snrt_dma_start_tracking();
                 // WARN: make sure that pointer types are according to network precision
-                float *act_ptr = ((uint32_t)activations) - cluster_offset;
+                float *act_ptr = ((uint32_t)activations_cl1) - cluster_offset;
                 float *img_ptr = ((uint32_t)images) - cluster_offset;
 
                 // for SSRs we need to DMA transfer the cluster 0 data to cluster 1
                 snrt_dma_txid_t txid_activations = 
-                    snrt_dma_start_1d(activations,                                 // destination
+                    snrt_dma_start_1d(activations_cl1,                                 // destination
                                     act_ptr,                                       // source
                                     n->dtype * n->OUT_CH);                         // size
 
@@ -284,7 +334,7 @@ void mnist_fp32(const network_fp32_t *n){
             volatile uint32_t ldB = compute_num;
             volatile uint32_t ldI = IN_CH;
 
-            float *act_ptr = ((uint32_t)activations) - cluster_offset;
+            float *act_ptr = ((uint32_t)activations_cl1) - cluster_offset;
             float *img_ptr = ((uint32_t)images) - cluster_offset;
 
             if(RUN_GRADIENT_UPDATE){
@@ -295,8 +345,8 @@ void mnist_fp32(const network_fp32_t *n){
                     // INFO: baseline
                     benchmark_get_cycle();
                     gradient_update_fp32n(n->IN_CH1, n->IN_CH2, div, 
-                                        &weights[W_offset], ldW, 
-                                        &biases[b_offset], &act_ptr[b_offset], 
+                                        &weight_grads_cl1[W_offset], ldW, 
+                                        &bias_grads_cl1[b_offset], &activations_cl1[b_offset], 
                                         ldB, &img_ptr[curr_img], &targets[curr_img], ldI, compute_id, 
                                         loss, compute_num);
                     benchmark_get_cycle();
@@ -304,8 +354,8 @@ void mnist_fp32(const network_fp32_t *n){
                     // INFO: FP64 with SSRs
                     benchmark_get_cycle();
                     gradient_update_fp32_ssr_simdn(n->IN_CH1, n->IN_CH2, div, 
-                                            &weights[W_offset], ldW, 
-                                            &biases[b_offset], &activations[b_offset], 
+                                            &weight_grads_cl1[W_offset], ldW, 
+                                            &bias_grads_cl1[b_offset], &activations_cl1[b_offset], 
                                             ldB, &images[curr_img], &targets[curr_img], ldI, compute_id, 
                                             loss, compute_num, setup_SSR);
                     benchmark_get_cycle();
@@ -348,18 +398,18 @@ void mnist_fp32(const network_fp32_t *n){
             // Discuss with GIM how to do DMA benchmarking
             snrt_dma_start_tracking();
             // WARN: make sure that pointer types are according to network precision
-            float *weight_grad_ptr = ((uint32_t)weights) + cluster_offset;
-            float *bias_grad_ptr = ((uint32_t)biases) + cluster_offset;
-            // snrt_dma_txid_t txid_WG = 
-            //     snrt_dma_start_2d(weights,                // destination
-            //                     weight_grad_ptr,          // source
-            //                     n->dtype * IN_CH,         // size
-            //                     n->dtype * IN_CH ,        // destination stride
-            //                     n->dtype * IN_CH ,        // source stride
-            //                     n->OUT_CH);               // repetitions
+            float *weight_grad_ptr = ((uint32_t)weight_grads_cl0) + cluster_offset;
+            float *bias_grad_ptr = ((uint32_t)biases_cl0) + cluster_offset;
+            snrt_dma_txid_t txid_WG = 
+                snrt_dma_start_2d(weight_grads_cl0,                // destination
+                                weight_grad_ptr,          // source
+                                n->dtype * IN_CH,         // size
+                                n->dtype * IN_CH ,        // destination stride
+                                n->dtype * IN_CH ,        // source stride
+                                n->OUT_CH);               // repetitions
 
             snrt_dma_txid_t txid_BG = 
-                snrt_dma_start_1d(activations,            // destination
+                snrt_dma_start_1d(activations_cl0,            // destination
                                 bias_grad_ptr,            // source
                                 n->dtype * n->OUT_CH);    // size
 
@@ -394,8 +444,8 @@ void mnist_fp32(const network_fp32_t *n){
         volatile uint32_t ldB = compute_num;
         volatile uint32_t ldI = IN_CH;
 
-        float *weight_grad_ptr = ((uint32_t)weights) + cluster_offset;
-        float *bias_grad_ptr = ((uint32_t)biases) + cluster_offset;
+        float *weight_grad_ptr = ((uint32_t)weight_grads_cl0) + cluster_offset;
+        float *bias_grad_ptr = ((uint32_t)biases_cl0) + cluster_offset;
 
         //TODO: load the LR from the network struct or via DRAM perloading
         //*learning_rate = 0.5;
@@ -410,16 +460,16 @@ void mnist_fp32(const network_fp32_t *n){
                 // INFO: baseline
                 benchmark_get_cycle();
                 training_step_fp32n(n->IN_CH1, n->IN_CH2, div, 
-                                            &weights[W_offset], &weight_grad_ptr[W_offset], ldW, 
-                                            &biases[b_offset], &bias_grad_ptr[b_offset], ldB, 
+                                            &weights_cl0[W_offset], &weight_grads_cl0[W_offset], ldW, 
+                                            &biases_cl0[b_offset], &activations_cl0[b_offset], ldB, 
                                             compute_id, compute_num, number_of_images);
                 benchmark_get_cycle();
             } else {
                 // INFO: FP64 with SSRs
                 benchmark_get_cycle();
                 training_step_fp32_ssr_simdn(n->IN_CH1, n->IN_CH2, div, 
-                                            &weights[W_offset], &weight_grad_ptr[W_offset], ldW, 
-                                            &biases[b_offset], &activations[b_offset], ldB, 
+                                            &weights_cl0[W_offset], &weight_grads_cl0[W_offset], ldW, 
+                                            &biases_cl0[b_offset], &activations_cl0[b_offset], ldB, 
                                             compute_id, compute_num, number_of_images, setup_SSR);
                 benchmark_get_cycle();
             }
