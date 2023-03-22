89b7b76a6dbeb0fa68f40a269668f34953aaa8d9
vivianep <vivianep@iis.ee.ethz.ch>
2023-03-22 13:50:58 +0100

    banshee: make DRAM preloading generic

--- sw/banshee/src/main.rs
+++ sw/banshee/src/main.rs
@@ -159,42 +159,22 @@ fn main() -> Result<()> {
                 .multiple(true)
                 .help("Pass command line arguments to LLVM"),
         )
-        // INFO: VIVI edit
+        // Replace single arguments with lists of arguments.
         .arg(
-            Arg::with_name("train-data-file-path")
-                .long("train-data-file-path")
+            Arg::with_name("file-paths")
+                .long("file-paths")
                 .takes_value(true)
-                .help("Path to data files (without labels) for training."),
+                .multiple(true)
+                .value_delimiter(",")
+                .help("Path to the binary data files (without labels) for training."),
         )
         .arg(
-            Arg::with_name("train-labels-file-path")
-                .long("train-labels-file-path")
+            Arg::with_name("mem-offsets")
+                .long("mem-offsets")
                 .takes_value(true)
-                .help("Path to the labels of the data for training."),
-        )
-        .arg(
-            Arg::with_name("train-data-bin-file-path")
-                .long("train-data-bin-file-path")
-                .takes_value(true)
-                .help("Path to the binary file of the data for training."),
-        )
-        .arg(
-            Arg::with_name("train-data-mem-offset")
-                .long("train-data-mem-offset")
-                .takes_value(true)
-                .help("Define the offset at which the train data should be written into DRAM."),
-        )
-        .arg(
-            Arg::with_name("train-labels-bin-file-path")
-                .long("train-labels-bin-file-path")
-                .takes_value(true)
-                .help("Path to the binary file of the labels for training."),
-        )
-        .arg(
-            Arg::with_name("train-labels-mem-offset")
-                .long("train-labels-mem-offset")
-                .takes_value(true)
-                .help("Define the offset at which the train labels should be written into DRAM."),
+                .multiple(true)
+                .value_delimiter(",")
+                .help("The memory offsets of the data files for training."),
         )
         .get_matches();
 
@@ -264,11 +244,7 @@ fn main() -> Result<()> {
     let has_num_cores = matches.is_present("num-cores");
     let has_num_clusters = matches.is_present("num-clusters");
     let has_base_hartid = matches.is_present("base-hartid");
-    // INFO: VIVI edit 
-    // let has_train_data = matches.is_present("train-data-file-path");
-    // let has_train_labels = matches.is_present("train-labels-file-path");
-    let has_train_bin = matches.is_present("train-data-bin-file-path");
-    let has_train_labels_bin = matches.is_present("train-labels-bin-file-path");
+    let has_bin_files = matches.is_present("file-paths");
 
     matches
         .value_of("num-cores")
@@ -324,110 +300,48 @@ fn main() -> Result<()> {
         .translate_elf(&elf)
         .context("Failed to translate ELF binary")?;
 
-    // INFO: VIVI edit --> approved
-    /* Preload the DRAM with the dataset 
-    *  provided in the SnitchUtilities CMAKE
-    *  file.
-    */
-    // First we check whether a file containing the training data (without labels)
-    // has been provided.
-    /*if has_train_data {
-        trace!("Entering DRAM preloading");
-        let train_data_path = matches.value_of("train-data-file-path").unwrap();
-        // we save the contents of the file or throw an error if something went wrong
-        // TODO: find more efficient alternative to read string
-        let train_data_contents = fs::read_to_string(train_data_path)
-            .expect("Something went wrong reading the file");
-        // // now we read in the contents into a HashMap<u64, u32> as <address, value> pair
-        let train_data = readmem::<u32>(&train_data_contents, ContentType::Float).unwrap();
-        // get memory handle, mutex thread lock, unwrap checks if element found, otherwise panics
-        // memory is defined as HashMap --> memory: Mutex<HashMap<u64, u32>>
-        // where the first value corresponds to the address 
-        let mut mem = engine.memory.lock().unwrap();
-        // we will extend the DRAM by the values we just read in
-        trace!("Starting to write the training data to DRAM");
-        mem.extend(train_data);
-        for i in 0x80040000..0x80104000 {
-            let val = mem.get(&(i)).copied().unwrap_or(0);
-            let float = unsafe { std::mem::transmute::<u32, f32>(val) };
-            trace!("address = 0x{:x}, original value = {:b}, float value = {}", i, val, float);
+    if has_bin_files {
+        // loop through the file paths
+        let file_paths = matches.values_of("file-paths").unwrap();
+        debug!("File paths: {:?}", file_paths);
+        for file_path in matches.values_of("file-paths").unwrap() {
+            debug!("loop File path: {:?}", file_path);
         }
-    }*/
-
-    // First we check whether a file containing the training data (without labels)
-    // has been provided.
-    /*if has_train_labels {
-        trace!("Loading labels");
-        let train_labels_path = matches.value_of("train-labels-file-path").unwrap();
-        // we save the contents of the file or throw an error if something went wrong
-        let train_labels_contents = fs::read_to_string(train_labels_path)
-            .expect("Something went wrong reading the file");
-        // now we read in the contents into a HashMap<u64, u32> as <address, value> pair
-        let train_labels = readmem::<u32>(&train_labels_contents, ContentType::Hex).unwrap();
-        // get memory handle, mutex thread lock, unwrap checks if element found, otherwise panics
-        // memory is defined as HashMap --> memory: Mutex<HashMap<u64, u32>>
-        // where the first value corresponds to the address 
-        let mut mem = engine.memory.lock().unwrap();
-        // we will extend the DRAM by the values we just read in
-        trace!("Starting to write the training labels to DRAM");
-        mem.extend(train_labels);
-        // for (k, v) in mem.iter(){
-        //     trace!("address = 0x{:x}, HEX value = 0x{:x}, value = {}", k, v, v);
-        // }
-        for i in 0x80108000..0x80108400 {
-            let val:u32 = mem.get(&(i)).copied().unwrap_or(0);
-            // let float = unsafe { std::mem::transmute::<u32, f32>(val) };
-            trace!("address = 0x{:x}, HEX value = 0x{:x}, label value = {}", i, val, val);
+        // iterate through the memory offsets
+        for mem_offset in matches.values_of("mem-offsets").unwrap() {
+            debug!("loop Memory offset: {:?}", mem_offset);
         }
-    }*/
+        let mem_offsets = matches.values_of("mem-offsets").unwrap();
+        debug!("Memory offsets: {:?}", mem_offsets);
 
-    if has_train_bin {
+        // check if the number of files and offsets are the same
+        if file_paths.len() != mem_offsets.len() {
+            bail!("Number of files and offsets are not the same. Cannot allocate memory.");
+        }
 
-        
-        let bin_path = matches.value_of("train-data-bin-file-path").unwrap();
-        
-        trace!("Loading train data from binary file: {}", bin_path);
+        // loop through the files and offsets
+        for (file_path, mem_offset) in file_paths.zip(mem_offsets) {
+            trace!("Loading binary data from file: {} and storing at memory offset: {}", file_path, mem_offset);
+            debug!("Loading binary data from file: {} and storing at memory offset: {}", file_path, mem_offset);
+            // get memory offset from argument
+            let mut memory_offset = mem_offset.trim_start_matches("0x");
+            // turn the string into a u64
+            let mut mem_offset = u64::from_str_radix(memory_offset, 16).unwrap();
 
-        // get memory offset from argument
-        let mut memory_offset = matches.value_of("train-data-mem-offset").unwrap().trim_start_matches("0x");
-        // turn the string into a u64
-        let mut mem_offset = u64::from_str_radix(memory_offset, 16).unwrap();
+            let data = dram_preload::generic_bin_read::<4>(file_path, mem_offset).unwrap();
+                
+            let data_length = data.len() as u64;
 
-        trace!("Train data starts at address: 0x{:x}", mem_offset);
-
-        let train_data = dram_preload::generic_bin_read::<4>(bin_path, mem_offset).unwrap();
+            let mut mem = engine.memory.lock().unwrap();
             
-        let train_data_length = train_data.len() as u64;
-
-        let mut mem = engine.memory.lock().unwrap();
-        
-        mem.extend(train_data);
-
-        for addr in mem_offset .. mem_offset + train_data_length {
-            let val:u32 = mem.get(&(addr)).copied().unwrap_or(0);
-            trace!("address = 0x{:x}, binary value = {:#034b}", addr, val);
+            mem.extend(data);
+            for addr in mem_offset..mem_offset + data_length {
+                let val:u32 = mem.get(&(addr)).copied().unwrap_or(0);
+                trace!("address = 0x{:x}, binary value = {:#034b}", addr, val);
+            }
         }
 
     }
-
-    if has_train_labels_bin {
-        let bin_path = matches.value_of("train-labels-bin-file-path").unwrap();
-        trace!("Loading train labels from binary file: {}", bin_path);
-        // get memory offset from argument
-        let mut memory_offset = matches.value_of("train-labels-mem-offset").unwrap().trim_start_matches("0x");
-        // turn the string into a u64
-        let mut mem_offset = u64::from_str_radix(memory_offset, 16).unwrap();
-        trace!("Train labels starts at address: 0x{:x}", mem_offset);
-        let train_labels = dram_preload::generic_bin_read::<8>(bin_path, mem_offset).unwrap();
-        let train_labels_length = train_labels.len() as u64;
-        let mut mem = engine.memory.lock().unwrap();
-        mem.extend(train_labels);
-        for addr in mem_offset .. mem_offset + train_labels_length {
-            let val:u32 = mem.get(&(addr)).copied().unwrap_or(0);
-            trace!("address = 0x{:x}, binary value = {:#034b}", addr, val);
-        }
-    }
-
     // Write the module to disk if requested.
     if let Some(path) = matches.value_of("emit-llvm") {
         unsafe {
